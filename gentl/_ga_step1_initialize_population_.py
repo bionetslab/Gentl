9b176a57bf61c64ae05096614849e3abc386ed79
suryadipto <ssarkarmanipal@gmail.com>
2024-11-03 08:48:01 +0100

    ---

--- gentl/_ga_step1_initialize_population_.py
+++ gentl/_ga_step1_initialize_population_.py
@@ -1,8 +1,8 @@
 import random
 
-def _ga_step1_initialize_population_(goal, Np_cap, g):
+def _ga_step1_initialize_population_randomly_(goal, Np_initial, g):
     """
-       Initialize the population by generating Np_cap chromosomes.
+       Initialize the population by generating Np_initial chromosomes.
        Each gene value derived from gene pool g.
 
        Parameters:
@@ -16,53 +16,7 @@ def _ga_step1_initialize_population_(goal, Np_cap, g):
     p = []
     chromosome_length = len(goal)
 
-    for i in range (int(Np_cap)):
-        chromosome = []
-        for gene in range(chromosome_length):
-            chromosome.append(random.choice(g))
-        p.append(chromosome)
-    return p
-
-def _ga_step1_initialize_population_(goal, Np_cap, g):
-    """
-       Initialize the population by generating Np_cap chromosomes.
-       Each gene value derived from gene pool g.
-
-       Parameters:
-       goal: The target list(cancer ROI)
-       Np_cap: Population capacity
-       g: Gene pool, which contains values from two feature lists.
-
-       Returns:
-       p: The initialized population.[[],[],[]]
-    """
-    p = []
-    chromosome_length = len(goal)
-
-    for i in range (int(Np_cap)):
-        chromosome = []
-        for gene in range(chromosome_length):
-            chromosome.append(random.choice(g))
-        p.append(chromosome)
-    return p
-
-def _ga_step1_initialize_population_randomly_(goal, Np_cap, g):
-    """
-       Initialize the population by generating Np_cap chromosomes.
-       Each gene value derived from gene pool g.
-
-       Parameters:
-       goal: The target list(cancer ROI)
-       Np_cap: Population capacity
-       g: Gene pool, which contains values from two feature lists.
-
-       Returns:
-       p: The initialized population.[[],[],[]]
-    """
-    p = []
-    chromosome_length = len(goal)
-
-    for i in range (int(Np_cap)):
+    for i in range (int(Np_initial)):
         chromosome = []
         for gene in range(chromosome_length):
             chromosome.append(random.choice(g))
